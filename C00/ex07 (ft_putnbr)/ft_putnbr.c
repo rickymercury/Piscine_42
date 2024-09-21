0f650e4c672a5e4c24016fdb8ce5b1331976454d
rickymercury <ricardomedeirosx@gmail.com>
2024-09-21 19:20:16 +0100

    add files

--- C00/ex07 (ft_putnbr)/ft_putnbr.c
+++ C00/ex07 (ft_putnbr)/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: rickymercury <ricardomedeirosx@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 13:44:04 by rickymercur       #+#    #+#             */
-/*   Updated: 2024/09/21 19:17:48 by rickymercur      ###   ########.fr       */
+/*   Updated: 2024/09/21 19:20:07 by rickymercur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,18 @@ void ft_putnbr(int nb)
     if (nb == -2147483648)
     {
         write(1, "-2147483648", 11);
-		 ft_putchar('-');
+		ft_putchar('-');
         return;
     }
     if (nb < 0)
-	{
+    {
         ft_putchar('-');
         nb = -nb;
     }
-	if (nb >= 10) 
-	{
+    if (nb >= 10) 
+    {
         ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+        ft_putnbr(nb % 10);
     }
     else
 	    ft_putchar(nb % 10 + '0');
