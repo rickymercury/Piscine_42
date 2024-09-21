a863cdb5ec559dbd91bf265429fc7d0df6352f5c
rickymercury <ricardomedeirosx@gmail.com>
2024-09-21 11:16:49 +0100

    ft_print_alphabet from C00

---
+++ C00/ex01 (ft_print_alphabet)/ft_print_alphabet.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rickymercury <ricardomedeirosx@gmail.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 11:14:03 by rickymercur       #+#    #+#             */
+/*   Updated: 2024/09/21 11:16:07 by rickymercur      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write (1, &letter, 1);
+		letter++;
+	}
+}
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    ft_print_alphabet();
+    return (0);
+}
+*/
