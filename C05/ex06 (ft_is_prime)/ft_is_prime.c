cb07902178f0cc59a3bd7d153049294179a06475
rickymercury <ricardomedeirosx@gmail.com>
2024-09-24 21:21:48 +0100

    Add files

--- C05/ex06 (ft_is_prime)/ft_is_prime.c
+++ C05/ex06 (ft_is_prime)/ft_is_prime.c
@@ -6,7 +6,7 @@
 /*   By: rickymercury <ricardomedeirosx@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:47:19 by rickymercur       #+#    #+#             */
-/*   Updated: 2024/09/24 16:03:21 by rickymercur      ###   ########.fr       */
+/*   Updated: 2024/09/24 21:21:40 by rickymercur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int	ft_is_prime(int nb)
 
 	divisor = 2;
 	if (nb < 2)
-		return (0);
+	    return (0);
 	while (divisor <= nb / divisor)
 	{
 		if (nb % divisor == 0)
-			return (0);
+		    return (0);
 		divisor++;
 	}
 	return (1);
