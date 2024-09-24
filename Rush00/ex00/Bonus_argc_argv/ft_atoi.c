15413cc5afe169d1982dd9fb07ef44402a42bf71
rickymercury <ricardomedeirosx@gmail.com>
2024-09-25 00:29:37 +0100

    Add rush completed with bonus

--- Rush00/ex00/Bonus_argc_argv/ft_atoi.c
+++ Rush00/ex00/Bonus_argc_argv/ft_atoi.c
@@ -1,25 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rickymercury <ricardomedeirosx@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/25 00:04:49 by rickymercur       #+#    #+#             */
-/*   Updated: 2024/09/25 00:26:58 by rickymercur      ###   ########.fr       */
+/*   Created: 2024/09/25 00:25:40 by rickymercur       #+#    #+#             */
+/*   Updated: 2024/09/25 00:28:33 by rickymercur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-
-void	rush(int x, int y);
-int		ft_atoi(char *str);
-
-int	main(int argc, char **argv)
+int	ft_atoi(char *str)
 {
-	if (argc == 3)
-		rush(atoi(argv[1]), atoi(argv[2]));
-	else
-		rush(5, 5);
-	return (0);
+	int	i;
+	int	number;
+	int	sign;
+
+	i = 0;
+	number = 0;
+	sign = 1;
+	while ((str[i] == ' ') || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;    // sign = -sign
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		number = number * 10 + (str[i] - '0');
+		i++;
+	}
+	return (number * sign);
 }
