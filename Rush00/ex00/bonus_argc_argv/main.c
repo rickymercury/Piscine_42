d43d2881631e008be8dfbdea795f3cd4c5ad21a8
rickymercury <ricardomedeirosx@gmail.com>
2024-09-28 23:01:33 +0100

    Add files

--- Rush00/ex00/bonus_argc_argv/main.c
+++ Rush00/ex00/bonus_argc_argv/main.c
@@ -6,20 +6,33 @@
 /*   By: rickymercury <ricardomedeirosx@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 00:04:49 by rickymercur       #+#    #+#             */
-/*   Updated: 2024/09/25 00:26:58 by rickymercur      ###   ########.fr       */
+/*   Updated: 2024/09/28 22:59:24 by rickymercur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
 
 void	rush(int x, int y);
 int		ft_atoi(char *str);
 
 int	main(int argc, char **argv)
 {
-	if (argc == 3)
-		rush(atoi(argv[1]), atoi(argv[2]));
-	else
-		rush(5, 5);
+	int	x;
+	int	y;
+
+	if (argc != 3)
+	{
+		write(2, "Incorrect number of arguments\n", 30);
+		return (1);
+	}
+	x = ft_atoi(argv[1]);
+	y = ft_atoi(argv[2]);
+	if (x <= 0 || y <= 0)
+	{
+		write(2, "Arguments should be greater than 0\n", 35);
+		return (1);
+	}
+	rush(x, y);
 	return (0);
 }
